ff78468a0e4d34af42613b821929ceccc699c053
ammarahmad <ammar.ahmad6410@gmail.com>
2023-10-22 19:30:05 +0500

    ssr done to fetch posts

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,21 +1,21 @@
 function Banner() {
 	return (
-		<div className="flex justify-evenly items-center bg-yellow-400 border-y border-black py-14">
-			<div className="px-10 space-y-5">
-				<h1 className="text-5xl  lg:text-6xl max-w-xl font-serif">
+		<div className="flex justify-around items-center bg-yellow-400 border-y border-black py-14 md:py-20">
+			<div className="px-10 space-y-5 max-w-xl lg:max-w-2xl">
+				<h1 className="text-5xl lg:text-6xl xl:text-7xl font-serif">
 					<span className="underline decoration-black decoration-4">
 						Medium
 					</span>{" "}
 					is a place to write, read, and connect
 				</h1>
-				<h2>
+				<h2 className="font-semibold">
 					It&apos;s easy and free to post your thinking on any topic and connect
 					with millions of readers.
 				</h2>
 			</div>
 
 			<img
-				className="hidden md:pr-10 lg:pr-0 md:inline-flex md:h-32 lg:h-72"
+				className="hidden md:pr-10 lg:pr-0 md:inline-flex md:h-44 lg:h-72 xl:h-80"
 				src="https://seekicon.com/free-icon-download/medium-m_1.svg"
 				alt=""
 			/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import Head from "next/head";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
+interface Props {
+	posts: [Post];
+}
 
-export default function Home() {
+export default function Home({ posts }: Props) {
 	return (
 		<div className="">
 			<Head>
@@ -14,3 +19,25 @@ export default function Home() {
 		</div>
 	);
 }
+
+export const getServerSideProps = async () => {
+	const query = `*[_type == "post"]{
+    _id,
+    title,
+    author->{
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+	const posts = await sanityClient.fetch(query);
+
+	return {
+		props: {
+			posts,
+		},
+	};
+};
